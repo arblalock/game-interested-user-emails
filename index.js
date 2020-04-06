9ef2f3358797c96758dc7eadfa01af51ca3bed8c
Adam Blalock <adamrblalock@gmail.com>
2020-04-06 09:40:28 -0700

    setting up

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-// Deployment notes: 
-// STRIPE_PROD_KEY is the overall api key
-// STRIPE_PROD_EP_KEY is the webhook api key, get this from the dashboard
+import { MongoClient } from 'mongodb'
+const connectMongoDB = () => MongoClient.connect(process.env.MONGODB)
 
-exports.tabulaInstallments = async (request, response) => {
+exports.interestedUserEmails = async (request, response) => {
+    if (request.method !== "POST") {
+        return response.status(400).send(`Error`);
+    }
     const req_auth = request.headers['auth'];
-
     if(process.env.AUTH_KEY !== req_auth){
         return response.status(400).send(`Error`);
     }
 
-    return response.status(200).json({ status: "success" });
+    var emailPatt = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+
+    const emailValid = emailPatt.test(request.body.email)
+    if(emailValid === true){
+        return response.status(200).json({ status: "success" });
+    }else{
+        return response.status(400).send(`Error`);
+    }
+    
 };
+
